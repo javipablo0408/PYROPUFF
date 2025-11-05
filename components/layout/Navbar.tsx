9a42ed3ba228edc71579deda6a3e2e50908202cd
javipablo0408 <javipablo0408@gmail.com>
2025-11-05 14:41:49 +0100

    Update logo filename to logo.jpeg

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -125,14 +125,15 @@ export function Navbar() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center">
-              <Link href="/" className="flex items-center h-12 w-auto">
+              <Link href="/" className="flex items-center h-12">
                 <Image
                   src="/logo.jpeg"
                   alt="Pyro Puff"
-                  width={120}
+                  width={150}
                   height={48}
                   className="h-12 w-auto object-contain"
                   priority
+                  unoptimized={false}
                 />
               </Link>
             </div>
